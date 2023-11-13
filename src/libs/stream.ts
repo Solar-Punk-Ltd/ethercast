165f12955c87e6427daa009333044a8692b62c7c
Levente Kiss <levente.kiss@solarpunk.bzz>
2023-11-13 18:09:17 +0100

    chore: experimenting

--- src/libs/stream.ts
+++ src/libs/stream.ts
@@ -1,15 +1,13 @@
 import { BatchId, FeedManifestResult, FeedWriter, Reference as BeeReference, Bee } from '@ethersphere/bee-js';
 import { Wallet } from 'ethers';
-import { FFmpeg } from '@ffmpeg/ffmpeg';
-import { stringify } from 'hls-parser';
-import { MasterPlaylist, MediaPlaylist, Segment, Variant } from 'hls-parser/types';
 import { MantarayNode, Reference as MantarayReference, StorageSaver } from 'mantaray-js';
+import builder from 'xmlbuilder';
 
 import { getBee } from './bee';
 import { bytesToHexString, hexStringToBytes, stringToBytes } from '../utils/formatters';
-import { fetchFile, toBlobURL } from '@ffmpeg/util';
 
 export class Stream {
+  private INDEX_COUNTER = 1;
   // random key for testing
   private _privateKey = 'cb35ff5ec82b182ef2c5fcbcaeb92120b453a013b107e98a9b4d93c39ce3f1d7';
   // there are no conventions yet so I choose 10 for the storage
@@ -21,15 +19,12 @@ export class Stream {
   private _feedWriter: FeedWriter;
   private _mediaRecorder: MediaRecorder;
   private _mediaStream: MediaStream;
-  private _masterPlaylist: MasterPlaylist;
-  private _mediaPlaylist: MediaPlaylist;
-  private _ffmpeg: FFmpeg;
+  private _mpdManifest: string;
 
   constructor() {
     this._wallet = new Wallet(this._privateKey);
     this._mantaray = new MantarayNode();
     this._bee = getBee();
-    this._ffmpeg = new FFmpeg();
   }
 
   async start(stamp: BatchId): Promise<void> {
@@ -37,39 +32,36 @@ export class Stream {
       this._mediaStream = await navigator.mediaDevices.getUserMedia({
         video: {
           width: {
-            ideal: 320,
+            ideal: 640,
           },
           height: {
-            ideal: 240,
+            ideal: 480,
           },
-          frameRate: { ideal: 15 },
+          frameRate: { ideal: 30 },
         },
         audio: false,
       });
 
       this._mediaRecorder = new MediaRecorder(this._mediaStream, {
-        mimeType: 'video/webm; codecs=H264',
+        mimeType: 'video/webm; codecs=vp9',
       });
 
-      await this.loadFFmpeg();
       await this.initFeedWriter(stamp);
-      this.createMasterPlaylist();
-      this.createMediaPlaylist();
+      // this.creatMpdManifest();
 
-      const playlistResult = await this._bee.uploadData(stamp, stringify(this._masterPlaylist));
-      this._mantaray.addFork(
-        stringToBytes('master.m3u8'),
+      // const playlistResult = await this._bee.uploadData(stamp, this._mpdManifest);
+      /*    this._mantaray.addFork(
+        stringToBytes('Manifest.mpd'),
         hexStringToBytes(playlistResult.reference) as MantarayReference,
         {
-          'Content-Type': 'application/x-mpegURL',
-          Filename: 'master.m3u8',
+          'Content-Type': 'application/dash+xml',
+          Filename: 'Manifest.mpd',
         },
-      );
+      ); */
 
       this._mediaRecorder.ondataavailable = async (event) => {
         if (event.data.size > 0) {
           await this.uploadChunk(stamp, this._feedWriter, new Uint8Array(await event.data.arrayBuffer()));
-          this.stop();
         }
       };
 
@@ -85,100 +77,123 @@ export class Stream {
   };
 
   get playlistUrl() {
-    return `http://localhost:1633/bzz/${this._feedManifest.reference}/master.m3u8`;
+    return `http://localhost:1633/bzz/${this._feedManifest.reference}/manifest.mpd`;
   }
 
   private async uploadChunk(stamp: BatchId, feedWriter: FeedWriter, chunk: Uint8Array) {
-    let index = 0;
-    const chunkResult = await this._bee.uploadFile(stamp, chunk, `chunk${index}.webm`, {
-      contentType: 'video/webm',
-    });
-
-    this._ffmpeg.writeFile(
-      `chunk${index}.webm`,
-      await fetchFile(`http://localhost:1633/bzz/${chunkResult.reference}/chunk${index}.webm`),
-    );
-    await this._ffmpeg.exec(['-i', `chunk${index}.webm`, `chunk${index}.mp4`]);
-    /*    const fileData = await this._ffmpeg.readFile(`chunk${index}.webm`);
-    const data = new Uint8Array(fileData as ArrayBuffer); */
-    index++;
-    console.log('TESSEK!');
-    return;
+    const chunkResult = await this._bee.uploadData(stamp, chunk);
+    const chunkNumber = this.getChunkNumber();
 
     this._mantaray.addFork(
-      stringToBytes(`chunk${index}.webm`),
+      stringToBytes(`chunk_${chunkNumber}.webm`),
       hexStringToBytes(chunkResult.reference) as MantarayReference,
       {
         'Content-Type': 'video/webm',
-        Filename: `chunk${index}.webm`,
+        Filename: `chunk_${chunkNumber}.webm`,
       },
     );
-
-    const segment = new Segment({
-      uri: `chunk${index}.webm`,
-      duration: 1,
-    });
-    this._mediaPlaylist.segments.push(segment);
-    index++;
-
-    const playlistResult = await this._bee.uploadData(stamp, stringify(this._mediaPlaylist));
-    this._mantaray.addFork(
-      stringToBytes('playlist.m3u8'),
-      hexStringToBytes(playlistResult.reference) as MantarayReference,
-      {
-        'Content-Type': 'application/x-mpegURL',
-        Filename: 'playlist.m3u8',
-      },
-    );
+    console.log(chunkNumber);
 
     const savedMantaray = await this._mantaray.save(this.createSaver(stamp));
 
     await feedWriter.upload(stamp, bytesToHexString(savedMantaray) as BeeReference);
   }
 
-  private async initFeedWriter(stamp: string | BatchId) {
+  private creatMpdManifest() {
+    const isoNow = new Date().toISOString();
+
+    const manifestObj = {
+      // https://docs.unified-streaming.com/documentation/live/configure-dynamic-mpd.html#mpd-minimumupdateperiod
+      MPD: {
+        '@xmlns': 'urn:mpeg:dash:schema:mpd:2011',
+        '@xmlns:xlink': 'http://www.w3.org/1999/xlink',
+        '@xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+        '@xsi:schemaLocation':
+          'urn:mpeg:DASH:schema:MPD:2011 http://standards.iso.org/ittf/PubliclyAvailableStandards/MPEG-DASH_schema_files/DASH-MPD.xsd',
+        '@profiles': 'urn:mpeg:dash:profile:isoff-live:2011',
+        '@type': 'dynamic',
+        '@maxSegmentDuration': 'PT1S',
+        '@minimumUpdatePeriod': 'PT5S',
+        '@suggestedPresentationDelay': 'PT3S',
+        '@timeShiftBufferDepth': 'PT15.8S',
+        '@minBufferTime': 'PT3.9S',
+        '@availabilityStartTime': isoNow,
+        '@publishTime': isoNow,
+        ProgramInformation: {
+          Title: {
+            '#text': 'Swarm live stream',
+          },
+        },
+        Period: {
+          '@id': 'P0',
+          '@start': 'PT0S',
+          AdaptationSet: {
+            '@contentType': 'video',
+            '@par': '16:9',
+            '@maxWidth': '320',
+            '@minWidth': '320',
+            '@maxHeight': '240',
+            '@minHeight': '240',
+            '@maxFrameRate': '15',
+            '@segmentAlignment': 'true',
+            '@mimeType': 'video/webm',
+            '@startWithSAP': '1',
+            SegmentTemplate: {
+              '@media': '$RepresentationID$/$Number$.webm',
+              // '@initialization': '$RepresentationID$/init.webm',
+              '@duration': '2',
+              '@startNumber': '0',
+              '@availabilityTimeOffset': '10',
+            },
+            Representation: {
+              '@id': 'V300',
+              '@bandwidth': '300000',
+              '@width': '320',
+              '@height': '240',
+              '@sar': '1:1',
+              '@frameRate': '15',
+              '@codecs': 'vp9',
+            },
+          },
+        },
+      },
+    };
+
+    this._mpdManifest = builder.create(manifestObj).end({ pretty: true });
+  }
+
+  async initFeedWriter(stamp: string | BatchId) {
     this._feedManifest = await this._bee.createFeedManifest(stamp, 'sequence', this._topic, this._wallet.address);
     this._feedWriter = this._bee.makeFeedWriter('sequence', this._topic, this._privateKey);
   }
 
-  private async loadFFmpeg() {
-    const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.4/dist/umd';
-    this._ffmpeg.on('log', ({ message }) => {
-      console.log(message);
-    });
-    // toBlobURL is used to bypass CORS issue, urls with the same
-    // domain can be used directly.
-    await this._ffmpeg.load({
-      coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
-      wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
-    });
-  }
-
-  private createMasterPlaylist() {
-    this._masterPlaylist = new MasterPlaylist({
-      variants: [
-        new Variant({
-          bandwidth: 800000,
-          uri: 'playlist.m3u8',
-          codecs: 'H264',
-          resolution: '320*240',
-        }),
-      ],
-    });
-  }
-
-  private createMediaPlaylist() {
-    this._mediaPlaylist = new MediaPlaylist({
-      targetDuration: 1,
-      version: 4,
-      segments: [],
-    });
-  }
-
-  private createSaver(stamp: string | BatchId): StorageSaver {
+  createSaver(stamp: string | BatchId): StorageSaver {
     return async (data: Uint8Array) => {
       const { reference } = await this._bee.uploadData(stamp, data);
       return hexStringToBytes(reference) as MantarayReference;
     };
+  }
+
+  private getChunkNumber() {
+    const specificTime = new Date('2023-11-13T00:00:00Z');
+    const currentTime = new Date();
+    const differenceInMilliseconds = currentTime.getTime() - specificTime.getTime();
+    return Math.round(differenceInMilliseconds / 1000);
+  }
+
+  private incrementCounter() {
+    this.INDEX_COUNTER++;
+  }
+
+  get mantaray() {
+    return this._mantaray;
+  }
+
+  get feedWriter() {
+    return this._feedWriter;
+  }
+
+  get feedManifest() {
+    return this._feedManifest;
   }
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,42 +1,76 @@
-import React, { useCallback, useMemo } from 'react';
-import Hls from 'hls.js';
+import React, { useCallback, useMemo, useState } from 'react';
+import { Reference as BeeReference } from '@ethersphere/bee-js';
+import { Reference as MantarayReference } from 'mantaray-js';
+import dashjs from 'dashjs';
+import { useDropzone } from 'react-dropzone';
+import styled from 'styled-components';
 
 import './Home.scss';
 import { useStamp } from '../hooks/useStamp';
 import { Stream } from '../libs/stream';
+import { getStamp } from '../libs/stamp';
+import { getBee } from '../libs/bee';
+import { bytesToHexString, hexStringToBytes, stringToBytes } from '../utils/formatters';
+
+const StyledDropzone = styled.div`
+  display: flex;
+  justify-content: center;
+  cursor: pointer;
+  color: white;
+  border: 1px solid white;
+  width: 500px;
+  padding: 4px 16px;
+  border-radius: 4px;
+  margin-top: 40px;
+`;
 
 function Home() {
   const { stamp } = useStamp();
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const stream = useMemo(() => new Stream(), []);
 
   const play = useCallback(async () => {
-    if (Hls.isSupported()) {
-      const video = document.getElementById('video') as HTMLMediaElement;
-      const hls = new Hls({ debug: true });
+    const video = document.getElementById('video') as HTMLMediaElement;
+    // console.log(stream.playlistUrl);
+    const src = `http://localhost:1633/bzz/3853d9987ea81b63f1873b1d50c0a7f2f2c374064e1a3c9ba08d0fe0fdab7982/manifest.mpd`;
+    const player = dashjs.MediaPlayer().create();
+    player.initialize(video, src, true);
+  }, []);
 
-      hls.on(Hls.Events.MEDIA_ATTACHED, function () {
-        console.log('video and hls.js are now bound together !');
-      });
+  const onDrop = useCallback(async (acceptedFiles: File[]) => {
+    setLoading(true);
 
-      hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
-        console.log('manifest loaded, found');
-      });
+    try {
+      const bee = getBee();
+      // do not care about multi select just grab the first one
+      const file = acceptedFiles[0];
+      const stamp = await getStamp();
 
-      hls.on(Hls.Events.ERROR, (event, data) => {
-        console.error(event);
-        console.error(data);
-        console.error('ERROR!!!!!');
+      const arrayBuffer = await file.arrayBuffer();
+      const data = new Uint8Array(arrayBuffer);
+
+      const { reference } = await bee.uploadData(stamp, data);
+      stream.mantaray.addFork(stringToBytes(file.name), hexStringToBytes(reference) as MantarayReference, {
+        'Content-Type': file.name.includes('mpd') ? 'application/dash+xml' : file.type,
+        Filename: file.name,
       });
-      /*    hls.loadSource(
-        'https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8',
-      ); */
-      hls.loadSource(stream.playlistUrl);
-      hls.attachMedia(video);
-      console.log(stream.playlistUrl);
-      // video.play();
+      console.log(file);
+      const savedMantaray = await stream.mantaray.save(stream.createSaver(stamp));
+
+      await stream.initFeedWriter(stamp);
+      await stream.feedWriter.upload(stamp, bytesToHexString(savedMantaray) as BeeReference);
+
+      console.log(`http://localhost:1633/bzz/${stream.feedManifest.reference}/${file.name}`);
+    } catch (error) {
+      setError(error as any);
+    } finally {
+      setLoading(false);
     }
   }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   return (
     <div className="home">
@@ -53,7 +87,18 @@ function Home() {
         </button>
       </div>
 
-      <video id="video" controls />
+      <video
+        id="video"
+        controls
+        style={{
+          marginTop: 20,
+        }}
+      />
+
+      <StyledDropzone {...getRootProps()}>
+        <input {...getInputProps({ disabled: true })} />
+        <p>Drag n drop a file here, or click to select file</p>
+      </StyledDropzone>
     </div>
   );
 }
