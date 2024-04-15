8494460d6f2757349c17ac44a5b15ae914f80f45
Roland Seres <roland.seres90@gmail.com>
2024-04-15 10:09:46 +0200

    UI related modifications

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 
 import './TextInput.scss';
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
 
 interface TextInputProps {
   label?: string;
@@ -10,15 +11,28 @@ interface TextInputProps {
   className?: string;
   value: string | number;
   name: string;
+  icon?: boolean;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   onKeyPress?: (event: React.KeyboardEvent<HTMLInputElement>) => void;
 }
 
-export function TextInput({ label, ...props }: TextInputProps) {
+export function TextInput({ label, icon, ...props }: TextInputProps) {
+  const [showIcons, setShowIcons] = React.useState(false);
+  function handleSmileyClick() {
+    setShowIcons(!showIcons);
+  }
   return (
-    <div className={clsx('text-input-container', props.className)}>
+    <div className={clsx('text-input-container')}>
       {label && <label className="text-input-label">{label}</label>}
+      {showIcons && (
+        <div className="text-input-icons">
+          <SentimentSatisfiedAltIcon />
+          <SentimentSatisfiedAltIcon />
+          <SentimentSatisfiedAltIcon />
+        </div>
+      )}
       <input className="text-input-field" {...props} />
+      {icon && <SentimentSatisfiedAltIcon className="text-input-icon" onClick={handleSmileyClick} />}
     </div>
   );
 }
