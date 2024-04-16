0bef4f79b2134ce4d26f82c98490f6c7bc1dbd79
Roland Seres <roland.seres90@gmail.com>
2024-04-16 16:26:07 +0200

    Code refactor

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -25,30 +25,8 @@ export function TextInput({ label, icon, ...props }: TextInputProps) {
   return (
     <div className={clsx('text-input-container')}>
       {label && <label className="text-input-label">{label}</label>}
-      {/* {showIcons && (
-        <div className="text-input-icons">
-          <EmojiPicker
-            previewConfig={{ showPreview: true }}
-            emojiSize={10}
-            showPreview={true}
-            width="100%"
-            theme="dark"
-            categories={[
-              { category: 'suggested', name: 'suggested' },
-              { category: 'smileys_people', name: 'Smileys & People' },
-              { category: 'animals_nature', name: 'Animal & Nature' },
-              { category: 'food_drink', name: 'Food & Drink' },
-              { category: 'travel_places', name: 'Travel & Places' },
-              { category: 'activities', name: 'Activities' },
-              { category: 'objects', name: 'Objects' },
-              { category: 'symbols', name: 'Symbols' },
-              { category: 'flags', name: 'Flags' },
-            ]}
-          />
-        </div>
-      )} */}
+
       <input className="text-input-field" {...props} />
-      {/* {icon && <SentimentSatisfiedAltIcon className="text-input-icon" onClick={handleSmileyClick} />} */}
     </div>
   );
 }
