0b850dabadabc4233c92806c68ffdf0e6bcff63b
Peter Ott <peter.ott@solarpunk.buzz>
2024-04-30 13:12:44 +0200

    Adds userSideReducer and functions

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -2,11 +2,12 @@ import { useEffect, useReducer, useState, createContext, useContext } from 'reac
 import { Controls } from './Controls/Controls';
 import { Message } from './Message/Message';
 import { TextInput } from '../TextInput/TextInput';
-import { MessageData, RoomID, getGraffitiFeedIndex, readSingleMessage } from '../../libs/chat';
+import { MessageData, RoomID, getGraffitiFeedIndex, readSingleMessage, registerUser } from '../../libs/chat';
 import { loadMessages, saveMessages, generateRoomId } from '../../utils/chat';
 import EditIcon from '@mui/icons-material/Edit';
 import './Chat.scss';
 import { MainContext } from '../../routes.tsx';
+import { chatUserSideReducer, initialStateForChatUserSide, readNextMessage } from '../../libs/chatUserSide.ts';
 
 export const LayoutContext = createContext({ chatBodyHeight: 'auto', setChatBodyHeight: (_: string) => {} });
 
@@ -14,44 +15,9 @@ interface ChatProps {
   feedDataForm: Record<string, any>;
 }
 
-interface ChatState {
-  messages: MessageData[];
-  readIndex: number;
-}
-
-type MessageAction =
-  | { type: 'insertMessage'; index: number; message: MessageData }
-  | { type: 'deleteMessage'; index: number }
-  | { type: 'incrementReadIndex' }
-  | { type: 'resetReadIndex' };
-
-function messagesReducer(state: { messages: MessageData[]; readIndex: number }, action: MessageAction): ChatState {
-  switch (action.type) {
-    case 'incrementReadIndex':
-      return { ...state, readIndex: state.readIndex + 1 };
-    case 'resetReadIndex':
-      return { ...state, readIndex: 0 };
-    case 'insertMessage':
-      const messages = [...state.messages];
-      messages[action.index] = action.message;
-      return { ...state, messages };
-    case 'deleteMessage':
-      return {
-        ...state,
-        messages: state.messages.filter((_, index) => index !== action.index),
-      };
-    default:
-      throw new Error();
-  }
-}
-
 export function Chat({ feedDataForm }: ChatProps) {
   const { mainNickName, setMainNickName } = useContext(MainContext);
-  const initialState: ChatState = {
-    messages: loadMessages(feedDataForm.topic.value),
-    readIndex: loadMessages(feedDataForm.topic.value).length,
-  };
-  const [state, dispatch] = useReducer(messagesReducer, initialState);
+  const [state, dispatch] = useReducer(chatUserSideReducer, initialStateForChatUserSide);
   const [initialized, setInitialized] = useState(false);
   const [chatBodyHeight, setChatBodyHeight] = useState('auto');
   const [nickname, setNickname] = useState(mainNickName); // Our name
@@ -67,19 +33,19 @@ export function Chat({ feedDataForm }: ChatProps) {
   useEffect(() => {
     if (initialized) {
       const messageChecker = setInterval(async () => {
-        await checkForMessages();
+        await readNextMessage(state, feedDataForm.topic.value, feedDataForm.address.value, dispatch);
       }, readInterval);
       return () => clearInterval(messageChecker);
     }
   }, [initialized]);
 
-  useEffect(() => {
-    readNextMessage();
-  }, [time]);
+  // useEffect(() => {
+  //   readNextMessage();
+  // }, [time]);
 
   // Init the chat application
   async function init() {
-    await readMessagesOnLoad();
+    // await readMessagesOnLoad();
     setInitialized(() => true);
   }
 
@@ -89,10 +55,12 @@ export function Chat({ feedDataForm }: ChatProps) {
     }
   };
 
-  const nicknameChoosed = () => {
+  const nicknameChoosed = async () => {
     if (nickname === '') {
       return;
     }
+    const result = await registerUser(feedDataForm.topic.value, feedDataForm.address.value, nickname, feedDataForm.stamp.value);
+        
     setIsNickNameSet(true);
     setMainNickName(nickname);
   };
@@ -105,102 +73,20 @@ export function Chat({ feedDataForm }: ChatProps) {
   }, [isEditMode]);
 
   // Reads those messags from Swarm, that does not exist in localStorage
-  async function readMessagesOnLoad() {
-    const roomId: RoomID = generateRoomId(feedDataForm.topic.value);
-    const feedIndex: number = Number(await getGraffitiFeedIndex(roomId));
+  // async function readMessagesOnLoad() {
+  //   const roomId: RoomID = generateRoomId(feedDataForm.topic.value);
+  //   const feedIndex: number = Number(await getGraffitiFeedIndex(roomId));
 
-    for (let i = state.messages.length; i < feedIndex; i++) {
-      const message = await readSingleMessage(i, roomId, feedDataForm.address.value);
-      dispatch({
-        type: 'insertMessage',
-        message: message,
-        index: i,
-      });
-      saveMessages(feedDataForm.topic.value, state.messages);
-    }
-  }
-
-  // Will update feedIndex, will do message fetch as well, if feed index changed.
-  async function checkForMessages() {
-    const roomId: RoomID = generateRoomId(feedDataForm.topic.value);
-
-    let feedIndex: number = Number(await getUpdateIndex(roomId));
-    if (state.readIndex > feedIndex) {
-      console.error('Warning! readIndex is higher then feedIndex, this should never happen!');
-      console.info('Setting readIndex to 0');
-      console.log('feedIndex: ', feedIndex);
-      console.log('readIndex: ', state.readIndex);
-      //dispatch({ type: 'resetReadIndex' });
-    }
-
-    if (feedIndex === -1) {
-      console.warn(
-        'Warning! getUpdateIndex gave back an error, possibly the feed is not ready yet. Retrying in 1 second',
-      );
-      return;
-    }
-
-    if (feedIndex > state.readIndex) {
-      console.log(`feedIndex > lastReadIndex | ${feedIndex} > ${state.readIndex}`);
-      console.log('Time: ', time);
-      setTime(() => Date.now());
-    }
-
-    if (feedIndex === -1) {
-      console.warn(
-        'Warning! getUpdateIndex gave back an error, possibly the feed is not ready yet. Retrying in 1 second',
-      );
-      return;
-    }
-
-    if (feedIndex > state.readIndex) {
-      console.log(`feedIndex > lastReadIndex | ${feedIndex} > ${state.readIndex}`);
-      console.log('Time: ', time);
-      setTime(() => Date.now());
-    }
-  }
-  
-  // Reads a single message, and will also save the messages to localStorage
-  async function readNextMessage() {
-    console.log('read with index ', state.readIndex);
-    const roomId: RoomID = generateRoomId(feedDataForm.topic.value);
-    let message: MessageData | null = null;
-    
-    do {
-      message = await readSingleMessage(state.readIndex, roomId, feedDataForm.address.value);
-      if (!message) {
-        console.error('Error reading message! Retrying...');
-        //sleep(1000);
-        //continue;
-        return;
-      }
-      
-      if (message.message) {
-        //setReadIndex(readIndex+1); Last                                 // Read was successful, but we don't know yet if it's duplicate or not
-        const isDuplicate = state.messages.some((msg) => msg.timestamp === message!.timestamp);
-        if (isDuplicate) {
-          // We won't insert this message, but lastReadIndex was already incremented
-          console.log('Duplicate!');
-          dispatch({ type: 'incrementReadIndex' });
-          return;
-        }
-        
-        dispatch({
-          type: 'insertMessage',
-          message: message,
-          index: state.readIndex,
-        });
-        dispatch({ type: 'incrementReadIndex' });
-        setTime(() => Date.now());
-        console.log('messages: ', state.messages);
-
-        // const uniqMessages = removeDuplicate([...messages, message]);       // Remove duplicate messages (by timestamp)
-        // const orderedMessages = orderMessages(uniqMessages);                // Order the messages, by timestamp
-        // setMessages(orderedMessages);                                       // Show the messages in the app
-        saveMessages(feedDataForm.topic.value, state.messages); // Save the messages to LocalStorage
-      }
-    } while (!message);
-  }
+  //   for (let i = state.messages.length; i < feedIndex; i++) {
+  //     const message = await readSingleMessage(i, roomId, feedDataForm.address.value);
+  //     dispatch({
+  //       type: 'insertMessage',
+  //       message: message,
+  //       index: i,
+  //     });
+  //     saveMessages(feedDataForm.topic.value, state.messages);
+  //   }
+  // }
 
 
   return (
@@ -249,7 +135,14 @@ export function Chat({ feedDataForm }: ChatProps) {
             )}
           </div>
         ) : (
-          <Controls topic={feedDataForm.topic.value} nickname={nickname} stamp={feedDataForm.stamp.value} />
+          <Controls 
+            topic={feedDataForm.topic.value}
+            streamerAddress={feedDataForm.address.value}
+            nickname={nickname}
+            stamp={feedDataForm.stamp.value}
+            state={state}
+            dispatch={dispatch}
+          />
         )}
       </div>
     </LayoutContext.Provider>
