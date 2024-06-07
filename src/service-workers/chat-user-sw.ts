aaffaa6eea628e6da9001a7649c883f3591296a8
Peter Ott <ottpeter@tuta.io>
2024-06-07 11:30:08 +0200

    chat-user-sw

--- src/service-workers/chat-user-sw.ts
+++ src/service-workers/chat-user-sw.ts
@@ -1,16 +1,75 @@
+import { EthAddress, readSingleMessage } from "../libs/chat";
+import { ChatActions, chatUserSideReducer, initialStateForChatUserSide } from "../libs/chatUserSide";
 const sw = self as unknown as ServiceWorkerGlobalScope;
+
+// State
+let state = initialStateForChatUserSide;
+
+// Input parameters
+let streamTopic: string | null = null;
+let streamerAddress: EthAddress | null = null;
+
+// Fetch interval in MS
+const FETCH_INTERVAL = 1000;
 
 // Listen for the 'install' event
 sw.addEventListener('install', _event => {
   sw.skipWaiting();
 });
   
-// Listen for the 'activate' event
+// Listen for the 'activate' event. This will reset the state.
 sw.addEventListener('activate', event => {
   // Cast event to ExtendableEvent to use waitUntil
   const activateEvent = event as ExtendableEvent;
   activateEvent.waitUntil(sw.clients.claim());
 
-  // Start fetching messages once the service worker is activated
-  START_FETCHING_MESSAGES();
+  state = initialStateForChatUserSide;
 });
+
+// Listen for messages from the main thread. Setting parameters, and reading the state.
+sw.addEventListener('message', event => {
+  const data = (event as MessageEvent).data;
+
+  // Handle different types of messages
+  switch(data.type) {
+    case 'SET_PARAMETERS':
+      streamTopic = data.streamTopic;
+      streamerAddress = data.streamerAddress;
+      startFetchingMessages();
+      break;
+    case 'GET_STATE':
+      (sw as unknown as Worker).postMessage({ type: 'STATE_UPDATE', state });
+      break;
+    default:
+      console.error('Unknown message type:', data.type);
+  }
+});
+
+// Will start the loop for fetching messages
+function startFetchingMessages() {
+  if (!streamTopic || !streamerAddress) {
+    console.error('Stream topic or streamer address not set');
+    return;
+  }
+  
+  setInterval(() => {
+    swReadNextMessage(streamTopic as string, streamerAddress as EthAddress);
+  }, FETCH_INTERVAL);
+}
+
+// swReadNextMessage will read a new message from the AggregatedFeed, and insert it into the state
+async function swReadNextMessage(streamTopic: string, streamerAddress: EthAddress) {
+  try {
+    const result = await readSingleMessage(state.chatIndex, streamTopic, streamerAddress);
+    if (!result) throw 'Error reading message!';
+
+    state = chatUserSideReducer(state, { type: ChatActions.ADD_MESSAGE, payload: { message: result } });
+    state = chatUserSideReducer(state, { type: ChatActions.ARRANGE });
+    state = chatUserSideReducer(state, { type: ChatActions.UPDATE_CHAT_INDEX, payload: { chatIndex: state.chatIndex + 1 } });
+
+  } catch (error) {
+    // Currently we can't distinguish "no new messages" from error
+    console.info("No new messages this time.");
+    return null;
+  }
+}
