a4566c0c7b02d9740bd58a803451d8d2b4ea1a85
Peter Ott <ottpeter@tuta.io>
2024-07-02 07:35:26 +0200

    new implementation of async queue

--- src/libs/asyncQueue.ts
+++ src/libs/asyncQueue.ts
@@ -1,16 +1,20 @@
+import { sleep } from '../utils/common';
 import { FIRST_SEGMENT_INDEX } from '../utils/constants';
 import { incrementHexString } from '../utils/operations';
 
 export class AsyncQueue {
-  private queue: ((index?: string) => Promise<void>)[] = [];
-  private indexed = false;
+  private indexed;
+  private waitable;
+  private clearWaitTime;
   private isProcessing = false;
+  private currentPromiseProcessing = false;
   private index = FIRST_SEGMENT_INDEX;
+  private queue: ((index?: string) => Promise<void>)[] = [];
 
-  constructor({ indexed }: { indexed: boolean }) {
-    this.queue = [];
-    this.isProcessing = false;
-    this.indexed = indexed;
+  constructor(settings: { indexed?: boolean; waitable?: boolean; clearWaitTime?: number } = {}) {
+    this.indexed = settings.indexed || false;
+    this.waitable = settings.waitable || false;
+    this.clearWaitTime = settings.clearWaitTime || 100;
   }
 
   private async processQueue() {
@@ -18,17 +22,31 @@ export class AsyncQueue {
     this.isProcessing = true;
 
     while (this.queue.length > 0) {
+      this.currentPromiseProcessing = true;
       const promise = this.queue.shift()!;
-      try {
-        if (this.indexed) {
-          await promise(this.index);
+      const action = this.indexed ? () => promise(this.index) : () => promise();
+
+      if (this.waitable) {
+        try {
+          await action();
           this.index = incrementHexString(this.index);
-        } else {
-          await promise();
+        } catch (error) {
+          console.error('Error processing promise:', error);
+          throw error;
+        } finally {
+          this.currentPromiseProcessing = false;
         }
-      } catch (error) {
-        console.error('Error processing promise:', error);
-        throw error;
+      } else {
+        action()
+          .then(() => {
+            this.index = incrementHexString(this.index);
+          })
+          .catch((error) => {
+            console.error('Error processing promise:', error);
+          })
+          .finally(() => {
+            this.currentPromiseProcessing = false;
+          });
       }
     }
 
@@ -38,5 +56,12 @@ export class AsyncQueue {
   enqueue(promiseFunction: (index?: string) => Promise<any>) {
     this.queue.push(promiseFunction);
     this.processQueue();
+  }
+
+  async clearQueue() {
+    this.queue = [];
+    while (this.isProcessing || this.currentPromiseProcessing) {
+      await sleep(this.clearWaitTime);
+    }
   }
 }
