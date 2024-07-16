0e448554d9f1f90b46e270acaf12ba83f0ab03d0
Peter Ott <ottpeter@tuta.io>
2024-07-16 11:41:09 +0200

    adds ethers and more comments

--- src/libs/chat/src/types.ts
+++ src/libs/chat/src/types.ts
@@ -2,6 +2,7 @@ import { HexString } from "@ethersphere/bee-js/dist/types/utils/hex";
 import { Signature } from "ethers";
 import { ETH_ADDRESS_LENGTH } from "./constants";
 
+// This is a hex string of specific length (42)
 export type EthAddress = HexString<typeof ETH_ADDRESS_LENGTH>;
 
 export interface ParticipantDetails {
@@ -11,13 +12,15 @@ export interface ParticipantDetails {
     stamp: string;
 }
 
+// Message object, contains the message, nickname that is not unique, an Ethereum address, and timestamp
 export interface MessageData {
     message: string;
     username: string;
     address: EthAddress;
     timestamp: number;
 }
-  
+
+// This is the object that is uploaded to the Graffiti-feed (Users feed)
 export interface User {
     username: string;
     address: EthAddress;
@@ -29,4 +32,5 @@ export interface UserWithIndex extends User {
     index: number;
 }
 
+// Where we use it, it is string. Will be used to create SHA hash
 export type Sha3Message = string | number[] | ArrayBuffer | Uint8Array;
